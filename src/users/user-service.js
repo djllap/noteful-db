9c946e5c133fddc468e46d8649e2e2c7721090fb
Daniel Kent <Kentpercussion@gmail.com>
2019-12-16 13:28:17 -0700

    added user service

---
+++ src/users/user-service.js
@@ -0,0 +1,35 @@
+const UserService = {
+  getAllUsers(knex) {
+    return knex.select('*').from('users');
+  },
+
+  insertUser(knex, newUser) {
+    return knex
+      .insert(newUser)
+      .into('users')
+      .returning('*')
+      .then(rows => {
+        return rows[0];
+      });
+  },
+
+  getById(knex, id) {
+    return knex
+      .select('*')
+      .from('users')
+      .where('id', id)
+      .first();
+  },
+
+  deleteUser(knex, id) {
+    return knex('users')
+      .where('id', id)
+      .delete();
+  },
+
+  updateUser(knex, id, newUserFields) {
+    return knex('users')
+      .where('id', id)
+      .update(newUserFields);
+  }
+};
